f4e11652b2796d2e63c2d523d4d86e19554a5897
isaac_dev <joseisaac.isaac123@gmail.com>
2026-01-22 09:59:45 -0300

    feat: inicializing prista setup and docker compose

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,20 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import type { SignInDTO, SignUpDTO } from './dtos/auth';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @Post('sign-in')
   async signIn(@Body() body: SignInDTO) {
-    console.log(body);
+    await this.authService.signIn(body);
+    return body;
   }
 
   @Post('sign-Up')
   async signUp(@Body() body: SignUpDTO) {
-    console.log(body);
-    
+    await this.authService.signUp(body);
+    return body;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import type { SignInDTO, SignUpDTO } from './dtos/auth';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async signUp(data: SignUpDTO){
+    return
+  }
+
+  async signIn(data:SignInDTO){
+    return
+  }
+}
