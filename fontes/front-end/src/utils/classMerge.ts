bdf5c36f6095383b13d8f5bebd6914ba96faacad
dev-matheusmarks <marques29matheus@edu.unifor.br>
2026-02-08 21:06:08 -0300

    Deletando módulo front-end

--- fontes/front-end/src/utils/classMerge.ts
+++
@@ -1,6 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function classMerge(...classes: ClassValue[]) {
-  return twMerge(clsx(classes));
-}
